418c5b4819227a6b55fffb50cdcffe03cf96c078
Kalley Matheus <matheusmonck@gmail.com>
2022-01-20 17:06:14 -0300

    ++

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Modal from "react-modal";
 
 import { GlobalStyle } from "./styles/global";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import { TransactionsContext } from "./TransactionsContext";
 
 Modal.setAppElement("#root");
 
@@ -18,7 +19,7 @@ export function App() {
     setisNewTransactionModalOpen(false);
   }
   return (
-    <>
+    <TransactionsContext.Provider value={[]}>
       <Header onOpenNewTransactionModal={handleOPenNewTransactionModal} />
       <Dashboard />
 
@@ -27,6 +28,6 @@ export function App() {
         onRequestClose={handleCloseNewTransactionModal}
       />
       <GlobalStyle />
-    </>
+    </TransactionsContext.Provider>
   );
 }
